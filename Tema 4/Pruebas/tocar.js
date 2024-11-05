ca7d7ed0fbba9d20e433191430ed1f1cef41b993
Jorge <georgiearclopez@gmail.com>
2024-11-05 10:38:03 +0100

    Prueba arreglada

--- Tema 4/Pruebas/tocar.js
+++ Tema 4/Pruebas/tocar.js
@@ -4,20 +4,19 @@ window.onload = function(){
 
     canvas.addEventListener('touchstart', (e) => {
         e.preventDefault();
-        console.log("Toque inicial");
+        canvas.innerHTML = "Toque inicial";
     });
-    
+
     canvas.addEventListener('touchmove', (e) => {
         e.preventDefault();
 
         const touch = e.touches[0];
         
-        console.log("Posicion x: " + touch.clientX);
-        console.log("Posicion y: " + touch.clientY);
+        canvas.innerHTML = "Posición x: " + touch.clientX + ", Posición y: " + touch.clientY;
     });
       
     canvas.addEventListener('touchend', () => {
-        console.log("Toque inicial");
+        canvas.innerHTML = "Toque inicial";
     });
 
 
